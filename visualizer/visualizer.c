6f1a0462f470a36574a91d0cee5da2a08dc76f4e
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-08-09 21:36:13 +0200

    Started work on ASCII visualizer

---
+++ visualizer/visualizer.c
@@ -0,0 +1,18 @@
+/**
+ * @author Andrej Bartulin
+ * PROJECT: 3x
+ * LICENSE: Public Domain
+ * DESCRIPTION: Main for ascii visualizer
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "gmp.h"
+
+int main()
+{
+    printf("Hello World!!\n");
+    return 0;
+}
